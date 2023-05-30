b87f34adb4679e2dd29ecbbd2140395f27d8dd2e
Onderito <ulas.onder@outlook.fr>
2023-05-30 15:52:15 +0200

    fin du projet Weather

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,86 @@
-import logo from '../../assets/logo.svg';
+import React, { useEffect, useState } from 'react';
+import { TextField, Slide, CircularProgress } from '@mui/material';
 
 import './App.scss';
 
 function App() {
+  const [cityName, setNameCity] = useState('Paris');
+  const [inputText, setInputText] = useState('');
+  const [data, setData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=02aeb6cec098cc655b156d3084cd659b&units=metric`
+    )
+      .then((res) => {
+        if (res.status === 200) {
+          error && setError(false);
+          return res.json();
+        } else {
+          throw new Error('Erreur, Oups');
+        }
+      })
+      .then((responseData) => {
+        setData(responseData);
+      })
+      .catch(() => setError(true))
+      .finally(() => setLoading(false));
+  }, [cityName, error]);
+
+  const handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      setNameCity(e.target.value);
+      setInputText('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="bg_img">
+      {!loading ? (
+        <>
+          <TextField
+            variant="filled"
+            label="recherche de location"
+            className="input"
+            error={error}
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            onKeyDown={handleSearch}
+          />
+          <h1 className="city">{data.name}</h1>
+          <div className="group">
+            <img
+              src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+              alt="logo"
+            />
+            <h1>{data.weather[0].main}</h1>
+          </div>
+          <h1 className="temp">{data.main.temp.toFixed()} °C</h1>
 
-        <p>
-          Edit <code>src/components/App/App.tsx</code> and save to reload.
-        </p>
+          <Slide direction="right" timeout={900} in={!loading}>
+            <div className="box_container">
+              <div className="box">
+                <p>Humidity</p>
+                <h1>{data.main.humidity.toFixed()}%</h1>
+              </div>
 
-        <a
-          className="App-link"
-          href="https://react.dev/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+              <div className="box">
+                <p>Wind</p>
+                <h1>{data.wind.speed.toFixed()}km/h</h1>
+              </div>
+
+              <div className="box">
+                <p>Feels Like</p>
+                <h1>{data.main.feels_like.toFixed()}°C</h1>
+              </div>
+            </div>
+          </Slide>
+        </>
+      ) : (
+        <CircularProgress />
+      )}
     </div>
   );
 }
